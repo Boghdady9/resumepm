45aeb8ee2f1091479304e876290094f62d37295d
Boghdady9 <MohamedBoghdady@my.uopeople.edu>
2024-11-30 14:17:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
 from data_loader import PDFTextProcessor
 from Agent import agent as create_agent
-import os
-from IPython.display import display, HTML
 
-# Create FastAPI instance
+# Load a resume PDF file
+processer=PDFTextProcessor("MohamedAbdalla_BEF.pdf")
+cv=processer.process()
 
-        # Process the PDF
-pdf_processor = PDFTextProcessor('Maha Jonas.pdf')
-resume_text = pdf_processor.process()
-
-# Pass the extracted text to the agent for further processing
-processed_output = create_agent(input="Can you enhance Professional Summary?"+resume_text)
-print(processed_output['output'])
-
+# The agent processes the resume and generates a new resume
+outcome=create_agent(cv)
